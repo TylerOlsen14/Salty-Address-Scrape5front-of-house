ae566bfb4f958a06abcb4235e17719e364b4a587
Tyler Olsen <Tyler.Olsen1@gmail.com>
2019-05-21 11:31:42 -0600

    changes to import address

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import './App.css';
-import NavBar from "./components/NavBar";
-import AddressCard from './components/AddressCard'
+// import NavBar from "./components/NavBar";
+// import AddressCard from './components/AddressCard'
+import NavBar from "src/components/NavBar";
+import AddressCard from 'src/components/AddressCard'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
